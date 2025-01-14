bf8ca539120275a6a34935464275cf9a0c2f7862
aman-pathan-josh <aman.pathan@joshsoftware.com>
2025-01-14 14:52:03 +0530

    Update

--- question5.rb
+++ question5.rb
@@ -1,19 +1,18 @@
-# 5. Write a program to find IP address class
-
 # Class A IP address : 0.16.12.34 to 127.52.36.11.
 # Class B IP address : 128.16.12.34 to 191.16.12.34.
 # Class C IP address : 192.16.12.34 to 233.168.1.15.
 # Class D IP address : 234.16.12.34 to 239.16.12.34.
 # Class E IP address : 240.16.12.34 to 255.16.12.34.
 
+ip_address_regex = /^([0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$/
 
 puts "Enter the IP Address :"
-input_string = gets
+input_string = gets.chomp
 
-if input_string =~ /^([0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3}[.][0-9]{1,3})$/
+if input_string.match(ip_address_regex)
     
-    first_substrings = input_string.split('.')
-    first_substring = first_substrings[0]
+    substrings = input_string.split('.')
+    first_substring = substrings[0]
 
     if first_substring.to_i >= 0 and first_substring.to_i <=127
         puts "Class A"
