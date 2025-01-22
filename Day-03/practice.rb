0b86b15fb6f961939020c76cd85cfa3f11d70938
aman-pathan-josh <aman.pathan@joshsoftware.com>
2025-01-22 12:07:53 +0530

    add practice.rb file

---
+++ Day-03/practice.rb
@@ -0,0 +1,71 @@
+class Car
+  def initialize(brand, model, year)
+    @brand = brand
+    @model = model
+    @year = year
+    @engine_number = generate_engine_number
+  end
+
+  def model
+    @model
+  end
+
+  def model=(new_model)
+    @model = new_model
+  end
+
+  def display_details
+    "Car Details:\nBrand: #{@brand}, Model: #{@model}, Year: #{@year}"
+  end
+
+  private
+
+  def generate_engine_number
+    "ENG-#{@model}-#{rand(100..1000)}"
+  end
+
+  public
+
+  def display_engine_number
+    "Car Engine Number: #{@engine_number}"
+  end
+
+  def self.car_class_method
+    "This is the class method."
+  end
+end
+
+car_obj = Car.new("Tata", "Tiago", 2020)
+puts car_obj.display_details
+
+print "\n"
+
+puts "Model: #{car_obj.model}"
+
+print "\n"
+
+car_obj = Car.new("Honda", "Civic", 2022)
+puts car_obj.display_details
+
+print "\n"
+
+puts car_obj.display_engine_number
+
+print "\n"
+
+puts Car.car_class_method
+
+
+# Output
+
+# Car Details:
+# Brand: Tata, Model: Tiago, Year: 2020
+
+# Model: Tiago
+
+# Car Details:
+# Brand: Honda, Model: Civic, Year: 2022
+
+# Car Engine Number: ENG-Civic-635
+
+# This is the class method.
